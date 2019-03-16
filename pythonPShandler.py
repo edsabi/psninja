8ab7962acbf62b1a807c91bcd2e123a8921a52c1
edsabi <38739707+edsabi@users.noreply.github.com>
2019-03-16 17:35:43 -0400

    Update pythonPShandler.py

--- pythonPShandler.py
+++ pythonPShandler.py
@@ -1,14 +1,26 @@
+import time
 cmd1 = ''
 while cmd1 != 'exit1':
 
+     	
+	
+    cmd1 = input('#:')
+
     with open('shell.txt', 'w') as ifile:
-        cmd1 = input('shell:')
+
         ifile.write(cmd1)
 
-    screen = ''
-    while screen != '1':
-        screen = input("Enter to Update")
 
-        if screen == '':
-            with open('shell2.txt', 'r') as read_file:
-                print(read_file.read().replace('\n\n','\n'))
+    time.sleep(4)
+
+    with open('shell2.txt', 'r') as read_file:
+        print(read_file.read().replace('\n\n','\n'))
+
+    with open('shell.txt', 'w') as ifile:
+
+        ifile.write('')
+
+	
+    with open('shell2.txt', 'w') as ifile:
+
+        ifile.write('')
